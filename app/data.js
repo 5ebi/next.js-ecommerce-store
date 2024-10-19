9f1d4b36ceba4b69f44ab1038b542b23a39ac2d9
Sebastian <sebi.speiser@gmx.net>
2024-10-19 17:06:16 +0200

    Add data with products

---
+++ app/data.js
@@ -0,0 +1,71 @@
+const productList = [
+  {
+    id: 1,
+    model: 'Profitec Go',
+    price: 900,
+    colors: [
+      { name: 'Black', image: '/images/profitecgo/go-black.jpg' },
+      { name: 'Blue', image: '/images/profitecgo/go-blue.jpg' },
+      { name: 'Brushed', image: '/images/profitecgo/go-brushed.jpg' },
+      { name: 'Red', image: '/images/profitecgo/go-red.jpg' },
+      { name: 'Yellow', image: '/images/profitecgo/go-yellow.jpg' },
+    ],
+  },
+  {
+    id: 2,
+    model: 'La Marzocco Linea Micra',
+    price: 3500,
+    colors: [
+      { name: 'Blue', image: '/images/lineamicra/micra-blue.jpg' },
+      { name: 'Red', image: '/images/lineamicra/micra-red.jpg' },
+      { name: 'Stainless Steel', image: '/images/lineamicra/micra-steel.jpg' },
+      { name: 'Yellow', image: '/images/lineamicra/micra-yellow.jpg' },
+    ],
+  },
+  {
+    id: 3,
+    model: 'Rocket Appartamento',
+    price: 1400,
+    colors: [
+      {
+        name: 'Copper',
+        image: '/images/rocketappartamento/appartamento-copper.jpg',
+      },
+      { name: 'Red', image: '/images/rocketappartamento/appartamento-red.jpg' },
+      {
+        name: 'White',
+        image: '/images/rocketappartamento/appartamento-white.jpg',
+      },
+      {
+        name: 'Black',
+        image: '/images/rocketappartamento/appartamento-black.jpg',
+      },
+    ],
+  },
+  {
+    id: 4,
+    model: 'Gaggia Classic Evo',
+    price: 450,
+    colors: [
+      { name: 'Green', image: '/images/gaggiaclassicevo/evo-green.jpg' },
+      { name: 'Red', image: '/images/gaggiaclassicevo/evo-red.jpg' },
+      { name: 'White', image: '/images/gaggiaclassicevo/evo-white.jpg' },
+      { name: 'Black', image: '/images/gaggiaclassicevo/evo-black.jpg' },
+    ],
+  },
+  {
+    id: 5,
+    model: 'Lelit Mara X',
+    price: 1250,
+    colors: [
+      {
+        name: 'Stainless Steel',
+        image: '/images/lelitmarax/mara-stainless.jpg',
+      },
+      { name: 'White', image: '/images/lelitmarax/mara-white.jpg' },
+      { name: 'Black', image: '/images/lelitmarax/mara-black.jpg' },
+    ],
+  },
+];
+
+export default productList;
--- app/products/page.js
+++ app/products/page.js
@@ -22,7 +22,31 @@ export default function Products() {
               <p> The Go is an outstanding price / value model.</p>
               <p> lorem ipsum.</p>
 
-              <button className={styles.buttonLearnMore}>Add to Cart</button>
+              {/* <button className={styles.buttonLearnMore}>Add to Cart</button> */}
+            </div>
+            <div>
+              <Image
+                src="/images/profitecgo/go-black.jpg"
+                width={473}
+                height={440}
+                alt="Picture of Profitec Go Black"
+                // style={imageStyle}
+              />
+            </div>
+          </div>
+        </Link>
+      </div>
+
+      <div className={styles.wrapperProducts}>
+        <Link href="/products/profitecGo" data-test-id="profitecGO-0001">
+          <div className={styles.wrapperProduct}>
+            <div className={styles.wrapperProductInfo}>
+              <h2>Profitec Go</h2>
+              <h3>Our bestseller </h3>
+              <p> The Go is an outstanding price / value model.</p>
+              <p> lorem ipsum.</p>
+
+              {/* <button className={styles.buttonLearnMore}>Add to Cart</button> */}
             </div>
             <div>
               <Image
